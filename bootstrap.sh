6d638572f456f4e3b3693d96c05f5a27303b177c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-03-13 19:43:29 -0500

    fix cargo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sudo pkg update && \
     sudo pkg install -y rust &&
-    sudo pkg clean -y
+    sudo pkg clean -y &&
+    sudo sed -i -e 's/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/local\/sbin \/usr\/local\/bin ~\/bin:\\/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/local\/sbin \/usr\/local\/bin ~\/bin \/home\/vagrant\/.cargo\/bin:\\/' /etc/login.conf &&
+    sudo cap_mkdb /etc/login.conf
